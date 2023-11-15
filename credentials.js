7cdfd90b551f518aac7406afbba258626cb80150
Gilson <gilsonfabio@gmail.com>
2023-11-15 11:02:25 -0300

    Alteração variaveis de ambiente

--- credentials.js
+++ credentials.js
@@ -1,8 +1,8 @@
 module.exports = {
 	// PRODUÇÃO = false
 	// HOMOLOGAÇÃO = true
-	sandbox: true,
-	client_id: 'Client_Id_6852dc0befc2f36fcbcbe85ebaaea7926292bfb3',
-	client_secret: 'Client_Secret_72c24f1aae9935f2300ebbeeaea165c6694c5563',
-	certificate: 'C:/bet/backbet/src/certs/homologacao-499441-NextBet.p12',
+	sandbox: process.env.EFIPAY_SANDBOX,
+	client_id: process.env.EFIPAY_CLIENT_ID,
+	client_secret: process.env.EFIPAY_CLIENT_SECRET,
+	certificate: process.env.EFIPAY_PATH_CERTIFICATE,
 }
