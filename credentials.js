a700e35b427b3a24bf0b0a49e8619b662679127b
Gilson <gilsonfabio@gmail.com>
2023-11-15 22:43:12 -0300

    Atualização da api pix credenciais

--- credentials.js
+++ credentials.js
@@ -1,8 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+const cert = fs.readFileSync(
+    path.resolve(__dirname, `src/certs/${process.env.EFIPAY_PATH_CERTIFICATE}`)	
+);
+
+//const cert = "C:/bet/backbet/src/certs/homologacao-499441-NextBet.p12"
+
+console.log('path do certificado:', cert);
 module.exports = {
 	// PRODUÇÃO = false
 	// HOMOLOGAÇÃO = true
 	sandbox: process.env.EFIPAY_SANDBOX,
 	client_id: process.env.EFIPAY_CLIENT_ID,
 	client_secret: process.env.EFIPAY_CLIENT_SECRET,
-	certificate: process.env.EFIPAY_PATH_CERTIFICATE,
+	certificate: cert, 
 }
