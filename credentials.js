4c9b7fde2b79a969561842386fb78ef0e6e1a8ce
Gilson <gilsonfabio@gmail.com>
2023-11-19 08:26:25 -0300

    Alteração no certificado

--- credentials.js
+++ credentials.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const path = require('path')
 
-const cert = fs.readFileSync(
-    path.resolve(__dirname, `src/certs/${process.env.EFIPAY_PATH_CERTIFICATE}`)	
-);
+//const cert = fs.readFileSync(
+//    path.resolve(__dirname, `src/certs/${process.env.EFIPAY_PATH_CERTIFICATE}`)	
+//);
 
 module.exports = {
 	// PRODUÇÃO = false
@@ -11,5 +11,5 @@ module.exports = {
 	sandbox: process.env.EFIPAY_SANDBOX,
 	client_id: process.env.EFIPAY_CLIENT_ID,
 	client_secret: process.env.EFIPAY_CLIENT_SECRET,
-	certificate: cert, 
+	certificate: 'C:/bet/backbet/src/certs/homologacao-499441-NextBet.p12', 
 }
